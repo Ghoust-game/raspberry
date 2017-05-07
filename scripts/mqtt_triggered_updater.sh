6ecbd15f989d9dca2325a27d0079a3bf375ddee4
trascen <s@w23.at>
2017-05-07 22:50:09 +0200

    only switch into rw mode once for the whole update

--- scripts/mqtt_triggered_updater.sh
+++ scripts/mqtt_triggered_updater.sh
@@ -4,12 +4,13 @@ set -e
 
 while true; do
 	if mosquitto_sub -C 1 -R -h localhost -p 1884 -t "GHOUST/server/perform-update"; then
+	    echo "Beginning update"
+	    rw
 	    for DIR in ["/server/webserver/frontend" "/server/ghoust"]; do
 		    cd "$DIR"
 
 		    TAG_LATEST=$( git tag -l | tail -n 1 )
 		    echo "Updating $DIR to $TAG_LATEST"
-		    rw
 
 		    echo "running 'git reset --hard' in $DIR"
 		    git reset --hard
@@ -17,11 +18,8 @@ while true; do
 
 		    echo "running 'git merge tags/$TAG_LATEST' in $DIR"
 		    git merge "tags/$TAG_LATEST"
-
-		    echo "setting drive to read-only"
-		    ro
-
-		    echo "Update successful."
-		done;
+	    done;
+	    echo "Ending update"
+	    ro
 	fi
 done;
